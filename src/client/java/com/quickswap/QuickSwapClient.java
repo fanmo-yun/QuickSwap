870528ae24a05eecb1f5d3388ebbce4e5aec2c08
fanmo-yun <xiaomojun9527@outlook.com>
2025-08-28 11:51:27 +0800

    Init project

---
+++ src/client/java/com/quickswap/QuickSwapClient.java
@@ -0,0 +1,10 @@
+package com.quickswap;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class QuickSwapClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
